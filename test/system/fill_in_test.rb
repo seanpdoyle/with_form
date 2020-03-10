077848e4065d17f8ab14625c9999eb7ca0cfdf73
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-10 01:01:21 -0400

    Introduce `WithForm::ModelForm`
    When submitting with an instance of an `ActiveModel::Model`, the
    `with_form` helper method will return an instance of
    `WithForm::ModelForm` instead of the pre-existing `WithForm::ScopeForm`.
    
    The `model:` option passed to the `WithForm::ModelForm` will be used to
    determine its [underlying `scope:` option][scope], which will be used to
    create an instance of the `WithForm::ScopeForm` that the `ModelForm`
    will delegate calls to.
    
    Unless they specify a value for `with:`, calls to `fill_in` will
    determine the value by delegating to the `model:` option instance.
    
    [scope]: https://api.rubyonrails.org/classes/ActiveModel/Naming.html

--- test/system/fill_in_test.rb
+++ test/system/fill_in_test.rb
@@ -1,7 +1,7 @@
 require "application_system_test_case"
 
 class FillInTest < ApplicationSystemTestCase
-  test "fill in form fields" do
+  test "fill in form fields with scope" do
     visit new_widget_path
 
     with_form(scope: :widget) do |form|
@@ -11,5 +11,37 @@ class FillInTest < ApplicationSystemTestCase
 
     assert_field translate("helpers.label.widget.text_field"), with: "text value"
     assert_field translate("helpers.label.widget.text_area_field"), with: "textarea value"
+  end
+
+  test "fill in form fields with model" do
+    widget = Widget.new(
+      text_field: "text value",
+      text_area_field: "textarea value",
+    )
+
+    visit new_widget_path
+    with_form(model: widget) do |form|
+      form.fill_in :text_field
+      form.fill_in :text_area_field
+    end
+
+    assert_field translate("helpers.label.widget.text_field"), with: "text value"
+    assert_field translate("helpers.label.widget.text_area_field"), with: "textarea value"
+  end
+
+  test "fill in form fields with overridden model" do
+    widget = Widget.new(
+      text_field: "text value",
+      text_area_field: "textarea value",
+    )
+
+    visit new_widget_path
+    with_form(model: widget) do |form|
+      form.fill_in :text_field, with: "text value override"
+      form.fill_in :text_area_field, with: "textarea value override"
+    end
+
+    assert_field translate("helpers.label.widget.text_field"), with: "text value override"
+    assert_field translate("helpers.label.widget.text_area_field"), with: "textarea value override"
   end
 end
