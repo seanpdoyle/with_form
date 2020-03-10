dee75e90fae281c5d61ab0e6c9db981988f2f4d3
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-10 08:23:32 -0400

    Add `WithForm::ModelForm#click_button`
    This commit adds `WithForm::ModelForm` implementation for its
    `WithForm::ScopeForm#click_button` counterpart.
    
    When invoked with a Symbol for `action`, translate the submit button
    label text with that Symbol as the action.
    
    Otherwise, the value of the submission depends on the state of the
    `model:` option:
    
    * Models that return `true` for calls to
      [`ActiveModel::Model#persisted?`][persisted] will translate with the
      `:update` key
    * Models that return `false` for calls to
      [`ActiveModel::Model#persisted?`][persisted] will translate with the
      `:create` key
    
    [persisted]: https://api.rubyonrails.org/classes/ActiveModel/Model.html#method-i-persisted-3F

--- test/system/click_button_create_test.rb
+++ test/system/click_button_create_test.rb
@@ -1,12 +1,38 @@
 require "application_system_test_case"
 
 class ClickButtonCreateTest < ApplicationSystemTestCase
-  test "click_button create" do
+  test "click_button create with scope" do
     visit new_widget_record_path
 
     with_form(scope: :widget_record) do |form|
       form.fill_in :text_field, with: "Created Record"
       form.click_button :create
+    end
+
+    assert_text "Created Record"
+    assert_equal ["Created Record"], WidgetRecord.pluck(:text_field)
+  end
+
+  test "click_button create with model" do
+    widget = WidgetRecord.new
+
+    visit new_widget_record_path
+    with_form(model: widget) do |form|
+      form.fill_in :text_field, with: "Created Record"
+      form.click_button
+    end
+
+    assert_text "Created Record"
+    assert_equal ["Created Record"], WidgetRecord.pluck(:text_field)
+  end
+
+  test "click_button create with overridable model" do
+    widget = WidgetRecord.new
+
+    visit new_widget_record_path
+    with_form(model: widget) do |form|
+      form.fill_in :text_field, with: "Created Record"
+      form.click_button :submit
     end
 
     assert_text "Created Record"
