06745228c056f0ae95e49861bbe40e4697647edd
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-10 07:54:45 -0400

    Add `WithForm::ModelForm#attach_file`
    This commit adds `WithForm::ModelForm` implementation for its
    `WithForm::ScopeForm#attach_file` counterpart.
    
    Unless the caller specifies a value for the `path` argument, calls to
    `attach_file` will determine the value by delegating to the `model:`
    option instance.

--- test/dummy/app/models/widget.rb
+++ test/dummy/app/models/widget.rb
@@ -2,6 +2,8 @@ class Widget
   include ActiveModel::Model
   include ActiveModel::Attributes
 
+
+  attribute :file_field
   attribute :select_field, :string
   attribute :multiple_select_field, :string
   attribute :text_field, :string
--- test/system/attach_file_test.rb
+++ test/system/attach_file_test.rb
@@ -1,10 +1,32 @@
 require "application_system_test_case"
 
 class AttachFileTest < ApplicationSystemTestCase
-  test "attach file" do
+  test "attach file with scope" do
     visit new_widget_path
 
     with_form(scope: :widget) do |form|
+      form.attach_file :file_field, file_fixture("file.txt")
+    end
+
+    assert_field translate("helpers.label.widget.file_field"), with: "C:\\fakepath\\file.txt"
+  end
+
+  test "attach file with model" do
+    widget = Widget.new(file_field: file_fixture("file.txt"))
+
+    visit new_widget_path
+    with_form(model: widget) do |form|
+      form.attach_file :file_field
+    end
+
+    assert_field translate("helpers.label.widget.file_field"), with: "C:\\fakepath\\file.txt"
+  end
+
+  test "attach file with overridable model" do
+    widget = Widget.new(file_field: file_fixture("other_file.txt"))
+
+    visit new_widget_path
+    with_form(model: widget) do |form|
       form.attach_file :file_field, file_fixture("file.txt")
     end
 
